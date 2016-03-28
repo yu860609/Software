865ba2f0b04ce81b8df1965dad46cac0f71f420c
Catherine Liu <catliu@mit.edu>
2016-03-28 19:09:40 -0400

    Start using Twist2DStamped according to ROS Diagram instead of WheelsCommandStamped in mdoap controllers. Not compatible with wheels_cmd_switch

--- catkin_ws/src/mdoap/src/mdoap_controller_node.py
+++ catkin_ws/src/mdoap/src/mdoap_controller_node.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import rospy
-from duckietown_msgs.msg import WheelsCmdStamped, BoolStamped, ObstacleProjectedDetectionList, ObstacleProjectedDetection
+from duckietown_msgs.msg import Twist2DStamped, BoolStamped, ObstacleProjectedDetectionList, ObstacleProjectedDetection
 
 class MDOAPControllerNode:
 	def __init__(self):
@@ -8,14 +8,14 @@ class MDOAPControllerNode:
         rospy.loginfo('[%s] started', self.name)
         self.sub_close = rospy.Subscriber("~too_close", BoolStamped, self.cbBool, queue_size=1)
         self.sub_detections = rospy.Subscriber("~detection_list_proj", ObstacleProjectedDetectionList, self.cbBool, queue_size=1)
-        self.pub_wheels_cmd = rospy.Publisher("~control",WheelsCmdStamped,queue_size=1)
+        self.pub_control = rospy.Publisher("~control",Twist2DStamped,queue_size=1)
 
     def cbBool(self, bool_msg):
-        stop = WheelsCmdStamped()
+        stop = Twist2DStamped()
         stop.header = bool_msg.header
-        stop.vel_left = 0.0
-        stop.vel_right = 0.0
-        self.pub_wheels_cmd.publish(stop)
+        stop.v = 0.0
+        stop.omega = 0.0
+        self.pub_control.publish(stop)
 
 if __name__=="__main__":
 	rospy.init_node('mdoap_controller_node')
